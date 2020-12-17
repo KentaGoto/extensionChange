e68a6903076e93fd8f08f32f29a607f982776a3d
KentaGoto <gotokentag@gmail.com>
2020-12-17 10:38:37 +0900

    1st

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func dirwalk(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, dirwalk(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join(dir, file.Name()))
+	}
+
+	return paths
+}
+
+func main() {
+	var dir string
+	var cExt string
+
+	if len(os.Args) != 3 {
+		fmt.Println("The number of arguments specified is incorrect.")
+		os.Exit(1)
+	} else {
+		dir = os.Args[1]  // Source directory
+		cExt = os.Args[2] // Extensions to be changed
+	}
+
+	paths := dirwalk(dir)
+
+	for _, path := range paths {
+		dirname := filepath.Dir(path)
+		filebasename := filepath.Base(path)
+		ext := filepath.Ext(path)
+
+		// Replace file extensions name
+		replacedFilebasename := strings.Replace(filebasename, ext, "."+cExt, 1)
+		if err := os.Rename(path, dirname+"\\"+replacedFilebasename); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Println("Done!")
+}
